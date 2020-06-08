088de35a0813fd602d977043765cec300e40a44e
ashumsd7 <40313523+ashumsd7@users.noreply.github.com>
2020-06-08 21:50:04 +0530

    updated all

--- app.js
+++ app.js
@@ -3,7 +3,16 @@ new Vue({
   
     data() {
         return {
-           time: "dd" 
+            partofspeech: [
+                'NOUN :   naming word',
+                'PRONOUN : replaces noun',
+                'VERB : action denoting word',
+                'ADJECTIVE :qualifies a noun/pronoun ',
+                'ADEVRB : qualifies a verb',
+                'PREPOSITION : relating word ',
+                'CONJUCTION : connectig word',
+                'INTERJECTION: Sudden Feeling Expression'
+            ]
         }
     },
     methods: {
@@ -13,7 +22,7 @@ new Vue({
       
     },
 
-   ,
+   
    
 
 })
